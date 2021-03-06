f8be50231d9db4fc65cb924b14bdb52f55bce9d8
Hawokaii <49223543+Hawokaii@users.noreply.github.com>
2021-03-06 22:09:44 +0600

    Add files via upload

---
+++ Studystorm-DML-data-load.sql
@@ -0,0 +1,19 @@
+.mode csv
+.separator ,
+.import students.csv students
+
+.mode csv
+.separator ,
+.import teachers.csv teachers
+
+.mode csv
+.separator ,
+.import course_schedule.csv course_schedule
+
+.mode csv
+.separator ,
+.import course_sections.csv course_sections
+
+.mode csv
+.separator ,
+.import course_selections.csv course_selections
